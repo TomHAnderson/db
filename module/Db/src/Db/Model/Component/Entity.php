44f46ed096b22f1bf138b1489960502091f9371c
Tom Anderson <tom.h.anderson@gmail.com>
2013-01-14 16:10:49 -0800

    Entity trait

---
+++ module/Db/src/Db/Model/Component/Entity.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Db\Model\Component;
+
+trait Entity
+{
+    abstract function getEntityName();
+
+    abstract function getDefaultSort();
+
+    public function find($id)
+    {
+        return $this->getEm()->getRepository($this->getEntityName())->find($id);
+    }
+
+    public function findOneBy($params)
+    {
+        return $this->getEm()->getRepository($this->getEntityName())->findOneBy($params);
+    }
+
+    public function findAll($sort = null)
+    {
+        if (!$sort) $sort = $this->getDefaultSort();
+        return $this->getEm()->getRepository($this->getEntityName())->findBy(array(), $sort);
+    }
+
+    public function findBy($search, $sort = null)
+    {
+        if (!$sort) $sort = $this->getDefaultSort();
+        return $this->getEm()->getRepository($this->getEntityName())->findBy($search, $sort);
+    }
+
+    public function findLike($search, $sort = null)
+    {
+        if (!$sort) $sort = $this->getDefaultSort();
+
+        $query = $this->getEm()->getRepository($this->getEntityName())->createQueryBuilder('s');
+        $i = 0;
+        foreach ($search as $column => $value) {
+            $paramName = md5(uniqid());
+            $query->andWhere('s.' . $column . ' LIKE ?' . ++$i);
+            $query->setParameter($i, $value);
+        }
+
+        foreach ($sort as $column => $direction) {
+            $query->add('orderBy', "s.$column $direction");
+        }
+
+        $query = $query->getQuery();
+
+       return $query->getResult();
+    }
+}
