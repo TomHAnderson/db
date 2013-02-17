4a4358e0c0e679948e8de849400cbf6b195ce7b5
Tom Anderson <tom.h.anderson@gmail.com>
2013-02-16 19:43:42 -0800

    Add icons to page names instead of titles

--- module/Jambase/config/module.config.php
+++ module/Jambase/config/module.config.php
@@ -3,11 +3,4 @@
 namespace Jambase;
 
 return array(
-    'di' => array(
-        'instance' => array(
-            'alias' => array(
-                'serviceJambase' => 'Jambase\Service\Jambase',
-            ),
-        ),
-    ),
 );
