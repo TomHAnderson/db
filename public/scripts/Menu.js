00b01a9307ca3f5ebf0a25c2c216cea1dd2f352a
Tom Anderson <tom.h.anderson@gmail.com>
2013-03-16 13:20:13 -0700

    Only allow brand to turn off site when logged in

--- public/scripts/Menu.js
+++ public/scripts/Menu.js
@@ -11,35 +11,6 @@ Menu = {
 
     bindUIActions: function()
     {
-        $('a#brand').on('turnOn', function(event) {
-            setCookie('editMode', 'on');
-            $(this).removeClass('btn-default').addClass('btn-warning');
-            $('a.btn-warning, a.btn-danger, a.btn-success, li#menu-audit').show();
-        });
-
-        $('a#brand').on('turnOff', function(event) {
-            setCookie('editMode', false);
-            $(this).removeClass('btn-warning').addClass('btn-default');
-            $('a.btn-warning, a.btn-danger, a.btn-success, li#menu-audit').hide();
-        });
-
-        $('a#brand').on('click', function(event) {
-            if ($(this).hasClass('btn-default')) {
-                $(this).trigger('turnOn');
-            } else {
-                $(this).trigger('turnOff');
-            }
-            $(this).show();
-
-            return false;
-        });
-
-        if (getCookie('editMode') == 'on') {
-            $('a#brand').trigger('turnOn');
-        } else {
-            $('a#brand').trigger('turnOff');
-        }
-
     },
 
     refresh: function()
